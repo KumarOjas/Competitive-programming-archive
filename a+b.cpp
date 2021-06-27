4fb3feab529b68cd65a33b8c990fee1dfc755d9a
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-06-27 23:24:04 +0530

    Create a+b.cpp

---
+++ a+b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a;
+  int b;
+  int t;
+  int i;
+  scanf("%d",&t);
+  for (i=0;i<t;i++)
+  {
+   scanf("%d",&a);
+   scanf("%d",&b);
+   printf("%d",a+b);
+   }
+  return 0;
+}
