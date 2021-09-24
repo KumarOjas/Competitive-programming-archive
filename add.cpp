ca852de92be31030829362ed189eb71e3696e491
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-09-24 12:21:12 +0530

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int T;
+  cin>>T;
+  while(T--)
+  {
+     int a,b;
+    cin>>a>>b;
+    int ans=a+b;
+    cout <<ans;
+  }
+  return 0;
+  
+  
+  }
+ 
+
+
+
+
+
+
+
+
+
+
+}
