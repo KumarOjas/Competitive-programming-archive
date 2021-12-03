8dda31c7d12da7d04b64ab4a6210f4cb6ac6efd7
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-03 12:21:10 -0800

    Create ADACRA.cpp

---
+++ ADACRA.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+
+int main () {
+  
+  long t;
+  cin>>t;
+  
+  while(t--)
+  {
+    long cnt(0);
+        for(long p = 1; p < s.size(); p++){if(s[p - 1] != s[p]){++cnt;}}
+        cnt = (cnt + 1) / 2;
+        printf("%ld\n", cnt);
+    }
+
+    return 0;
+} 
+ }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
