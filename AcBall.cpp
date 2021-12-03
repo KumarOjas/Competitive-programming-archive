63f1de2875f138a43e5cfaea904fbec656361a02
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-03 12:18:56 -0800

    Create AcBall.cpp

---
+++ AcBall.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main () {
+  
+  int t;
+  cin >> t;
+  
+  while(t--){
+  
+        string x, y; 
+        std::cin >> x >> y;
+        std::string z(x);
+        for(int p = 0; p < z.size(); p++){
+            if(x[p] == 'B' && y[p] == 'B'){z[p] = 'W';}
+            else if(x[p] == 'W' && y[p] == 'W'){z[p] = 'B';}
+            else{z[p] = 'B';}
+        }
+
+        std::cout << z << std::endl;
+    }
+
+    return 0;
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
