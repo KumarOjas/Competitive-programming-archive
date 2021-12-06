8138828f85ced8167d26d9e2b024b444e1ceda72
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-05 22:13:22 -0800

    Create Adaschool.cpp

---
+++ Adaschool.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+int main () {
+  
+  long t;
+  
+  scanf("%ld",&t);
+  
+  while(t--){
+    
+    long n,m;
+    
+    scanf("%ld%ld",&n,&m);
+  
+    puts(m*n%2?"No":"Yes");
+  
+  }
+ 
+  
+  return 0;
+}
+    
+    
+  
+  
+  
+  
+  
+  
