d2039a8e9b7216d4f3a095fb626e8f8125c0b4b1
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-06 20:36:42 -0800

    Create Amsgame1.cpp

---
+++ Amsgame1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+inline size gcd (size a ,size b)
+{
+
+return (b == 0) ? a : gcd (b, a%b);
+}
+
+int main () {
+  
+   size_t length, temp = 0, output = 0;
+    int numCases; scanf("%d\n",&numCases);
+    while(numCases--){
+        scanf("%zd\n", &length);
+        output = 0;
+        for(size_t k = 0; k < length; k++){
+            scanf("%zd", &temp);
+            output = gcd(output, temp);
+        }
+        printf("%zd\n",output);
+    }
+    return 0;
+}
