76e810d0cc64dc0a6e55a3aeb51e8d3f864df529
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-07 00:29:29 -0800

    Create Wsc.cpp

---
+++ Wsc.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include<iostream>
+#include <vector>
+
+int main(){
+  
+  long t;
+  scanf("%ld\n",&t);
+  
+   for
+     (
+     int p = 0; p < m; p++)
+   {
+      int x, y; scanf("%d %d\n", &x, &y);
+      a[p] = std::make_pair(x, y);
+    }
+
+        if(m < 2){puts("YES");}
+        else if(m > 2){puts("NO");}
+        else{
+            bool same = (a[0].first == a[1].first) ||(a[0].first == a[1].second) ||(a[0].second == a[1].first) ||(a[0].second == a[1].second);
+            puts(same ? "YES" : "NO");
+        }
+    }
+
+    return 0;
+}
+  
+  
+  
+  
+  
+  
+  
+  
