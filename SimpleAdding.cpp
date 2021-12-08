560a8acb08cfa674c58bd4e7e2b48355378a2a4d
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-08 00:57:36 -0800

    Create SimpleAdding.cpp

---
+++ SimpleAdding.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int SimpleAdding (int num )
+  
+{
+
+return num * (num + 1) / 2;
+
+}
+
+int main (){
+  
+  
+  cout << SimpleAdding(gets(stdin));
+  return 0;
+    
+}    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
