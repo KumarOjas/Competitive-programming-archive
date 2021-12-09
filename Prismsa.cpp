3940ff7c64f6c3430c2a24033221f1fdf658a285
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-09 03:42:01 -0800

    Create Prismsa.cpp

---
+++ Prismsa.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <cmath>
+
+int main () {
+  
+    int numCases; 
+  
+  scanf("%d\n",&numCases);
+ 
+  while(numCases--)
+  {
+        double V;
+    
+    scanf("%lf",&V);
+  
+    printf("%.10f\n", 3 * sqrt(3) * pow( 2 * V * V, 1.0/3.0));
+    }
+    return 0;
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
