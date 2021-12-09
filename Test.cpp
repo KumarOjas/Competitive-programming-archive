239d64e41ecdd1e73e8f2802f01fb4dec3912a0d
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-09 03:47:00 -0800

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namepsace std;
+
+
+
+int main
+(
+int argc , char *argv[]
+)
+{
+    int inputNumber = 0;
+    
+    const int answer = 42;
+
+    while(true)
+    {
+        cin  >> inputNumber;
+        if
+        (inputNumber == answer)
+        {break;}
+        
+        cout << inputNumber << endl;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
