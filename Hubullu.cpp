e57cb28704e2982bcf6dbfd2f3de05a833d6a9c8
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-09 03:02:41 -0800

    Create Hubullu.cpp

---
+++ Hubullu.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+
+int main () {
+  
+  
+  
+  int numCases;
+  
+  scanf("%d",&numCases);
+  
+  size_t N = 0;
+  
+  int first = 0;
+  
+  while
+    (numCases--)
+  {
+    
+    scanf("%zd %d\n",&N, &first);
+  
+    first ? printf("Pagfloyd wins.\n") : printf("Airborne wins.\n");
+    
+  }
+    return 0;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
