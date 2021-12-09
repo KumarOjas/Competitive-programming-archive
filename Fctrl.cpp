1cc1ab69e921459e1599b416de3ab74d3dfab8d3
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-09 02:47:39 -0800

    Create Fctrl.cpp

---
+++ Fctrl.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main 
+(
+int argc, char *argv[])
+{
+    size_t inputNumber, factor, output;
+    int N; std::cin >> N;
+
+    for(size_t k = 0; k < N ; k++){
+        cin >> inputNumber;
+        factor = 5, output = 0;
+        while(factor <= inputNumber){
+            output += inputNumber / factor;
+            factor *= 5;
+        }
+        std::cout << output << std::endl;
+    }
+    return 0;
+}
