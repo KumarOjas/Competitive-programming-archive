08edb1039297d7b585c48e9e599bed0c7ab3d648
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-09 00:57:44 -0800

    Create Bomarble.cpp

---
+++ Bomarble.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+int main () {
+  unsigned n;
+  while (1)
+  {
+        scanf("%u\n",&n); if(!n){break;}
+        unsigned long output = (n + 1) * (3*n + 2) / 2;
+        printf("%lu\n",output);
+    }
+    return 0;
+}
+    
+    
+    
+    
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
