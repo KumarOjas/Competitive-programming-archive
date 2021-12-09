2d3086dc849ca49b1b7e5eb12340ac7406d0b359
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-09 03:12:30 -0800

    Create Ngm.cpp

---
+++ Ngm.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main(){
+    size_t N;
+  scanf("%zd",&N);
+  
+  int output = N % 10;
+  
+  if(output != 0){printf("1\n%d\n",output);}
+  
+  else{printf("2\n");}
+  
+  return 0;
+}
