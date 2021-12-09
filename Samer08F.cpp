0f93aab58f262e54e6b3bc5c163d17449b6c9e6a
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-09 03:43:51 -0800

    Create Samer08F.cpp

---
+++ Samer08F.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+int main(){
+
+  size_t N;
+  
+  while(true)
+    {
+    
+      scanf("%zd",&N);
+      
+      if(N <= 0){break;}
+      
+      printf("%zd\n", N * (N + 1) * (2*N + 1)/6);
+    }
+
+    return 0;
+}
