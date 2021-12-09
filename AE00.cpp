81dcbabac8c8f36021edb2e38c8b46cced16d7a2
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-09 00:54:30 -0800

    Create AE00.cpp

---
+++ AE00.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <cmath>
+
+int main () {
+  int N;
+  scanf("%d",&N);
+  int output=0;
+  for (int k=1;k<=sqrt (N);k++){output += (N / k) - k + 1;}
+    printf("%d\n",output);
+    return 0;
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
