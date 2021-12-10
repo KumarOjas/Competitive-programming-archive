c1eaf9bbe20548f693e2d75083d8315372259357
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-09 23:49:15 -0800

    Create Fence1.cpp

---
+++ Fence1.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <cmath>
+
+int main () {
+  
+  int L = 0;
+ 
+  const double PI = 4 * atan(1);
+  
+  while(1)
+  {
+        scanf("%d", &L); if(L <= 0){break;}
+        printf("%.2f\n", L * L /(2 *PI));
+    }
+
+    return 0;
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
