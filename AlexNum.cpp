7f6140cd5ca37c68e8a9958800662de94a02c18b
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-12 08:19:36 -0800

    Create AlexNum.cpp

---
+++ AlexNum.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+
+int main () {
+  
+  int t;
+ 
+  scanf ("%d\n",&t);
+ 
+  while (t--)
+  {
+  
+    long long n;
+    
+    scanf("%ld\n"&n);
+    
+    for (long long p=0;p<n;p++)
+      {
+        long a;
+        scanf("%ld", &a);
+      }
+    printf("%lld\n", n * (n - 1) / 2);
+  }
+  return 0;
+}
+    
+    
+    
+    
+    
+    
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
